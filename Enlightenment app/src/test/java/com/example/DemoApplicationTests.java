8a1a48fd840f409b9ce8cda4f68d42325427655f
Per Schmidt <per@oodevelop.dk>
2018-05-08 06:35:57 +0200

    Added @SpringBootApplication to DemeApplicationTests

--- Enlightenment app/src/test/java/com/example/DemoApplicationTests.java
+++ Enlightenment app/src/test/java/com/example/DemoApplicationTests.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@SpringBootApplication
 public class DemoApplicationTests {
 
 	@Test
